4bbf41adbad2e83bbb2940b55bb49768ddbfba89
Fatball511 <81003272+Fatball511@users.noreply.github.com>
2022-12-07 13:55:48 +0000

    Add files via upload

---
+++ Lab02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 12:33:38 2022
+
+@author: keithcheng
+
+"""
+
+import pandas as pd
+
+p = pd.read_csv('passengerData.csv')
+t = pd.read_excel('ticketPrices.xlsx','Sheet1')
